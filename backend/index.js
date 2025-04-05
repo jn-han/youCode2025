2ba4fadfb9814b99344afff787e5ea5b9da8b501
Joshua Nguyen <joshuahnguyen@gmail.com>
2025-04-05 15:32:15 -0700

    User sign up implemented

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ require("dotenv").config(); // Load env variables
 const userRoutes = require("./routes/userRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors());
@@ -14,10 +14,6 @@ app.use(express.json());
 
 //Register routes
 app.use("/users", userRoutes);
-
-app.get("/test", (req, res) => {
-  res.send("Backend is alive!");
-});
 
 // MongoDB Connection
 mongoose
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { createUser } = require("../controllers/userController");
+const { createUser, getUsers } = require("../controllers/userController");
 
 // POST /users
 router.post("/", createUser);
+router.get("/", getUsers); // ✅ This defines GET /users
 
 module.exports = router;
