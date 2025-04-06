a4ba4c956d60b2d41c353d96f893ddb068c0d4fe
Joshua Nguyen <joshuahnguyen@gmail.com>
2025-04-05 23:25:58 -0700

    added fontawesome

--- frontend/src/app/SignUp/page.js
+++ frontend/src/app/SignUp/page.js
@@ -3,6 +3,12 @@ import React from "react";
 import { useState, useEffect } from "react";
 import API from "../../../utils/api";
 import dynamic from "next/dynamic";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGoogle,
+  faFacebook,
+  faApple,
+} from "@fortawesome/free-brands-svg-icons";
 
 const ClientSelect = dynamic(() => import("@/components/ClientSelect"), {
   ssr: false,
@@ -11,6 +17,8 @@ const ClientSelect = dynamic(() => import("@/components/ClientSelect"), {
 const SignUpPage = () => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+
   const [email, setEmail] = useState("");
   const [experiencedActivities, setExperiencedActivities] = useState([]);
   const [wantsToExperience, setWantsToExperience] = useState([]);
@@ -58,8 +66,62 @@ const SignUpPage = () => {
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-items-center w-full">
-      <p>User Sign Up</p>
+    <div className="flex flex-row items-center justify-center w-full bg-amber-800 h-screen">
+      <div className="w-1/3 bg-white flex flex-col items-center justify-center text-bold">
+        <p className="text-4xl m-2">Sign Up</p>
+        <p>Step 1/3</p>
+        <div className="w-4/5 flex flex-col  items-center jutify-center m-2">
+          <div className="w-3/4">
+            <p>Email</p>
+          </div>
+          <input
+            className="border p-3 text-xl w-3/4 border-black"
+            placeholder=""
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="w-4/5 flex flex-col  items-center jutify-center m-2">
+          <div className="w-3/4">
+            <p>Password</p>
+          </div>
+          <input
+            className="border p-3 text-xl w-3/4 border-black"
+            placeholder=""
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <hr className="w-2/4 h-px my-8 bg-black border-0 " />
+        <div className="flex flex-col justify-center  w-4/5 items-center">
+          <div className="p-2 m-2 w-3/4 border flex flex-row justify-center text-xl">
+            <p>
+              <span className="m-2">
+                <FontAwesomeIcon icon={faFacebook}></FontAwesomeIcon>
+              </span>
+              Sign in with Facebook
+            </p>
+          </div>
+          <div className="p-2 m-2 w-3/4 border flex flex-row justify-center text-xl">
+            <p>
+              <span className="m-2">
+                <FontAwesomeIcon icon={faGoogle}></FontAwesomeIcon>
+              </span>
+              Sign in with Google
+            </p>
+          </div>
+          <div className="p-2 m-2 w-3/4 border flex flex-row justify-center text-xl">
+            <p>
+              <span className="m-2">
+                <FontAwesomeIcon icon={faApple}></FontAwesomeIcon>
+              </span>
+              Sign in with Apple
+            </p>
+          </div>
+          <button className="w-1/2 p-4 text-xl border m-1">Next Step</button>
+        </div>
+      </div>
+
+      {/* <p>User Sign Up</p>
       <div className="flex flex-row justify-between w-100">
         <div className="">
           <p>First Name</p>
@@ -103,11 +165,11 @@ const SignUpPage = () => {
           loadOptions={loadOptions}
           topThreeOptions={topThreeOptions}
         />
-      </div>
+      </div> */}
 
-      <button className="font-primary" onClick={handleCreateUser}>
+      {/* <button className="font-primary" onClick={handleCreateUser}>
         Create User
-      </button>
+      </button> */}
     </div>
   );
 };
